ed0116e1ce6155b9db20bae23c5a8785d7c35314
phamcaochithanh <chithanhphamj@gmail.com>
2025-03-29 08:52:33 +0700

    Add lab02 Cipher

---
+++ Lab_02/Cipher/transposition/transposition_cipher.py
@@ -0,0 +1,30 @@
+class TranspositionCipher:
+    def __init__(self):
+        pass
+
+    def encrypt(self, text, key):
+        encrypted_text = ''
+        for col in range(key):
+            pointer = col
+            while pointer < len(text):
+                encrypted_text += text[pointer]
+                pointer += key
+        return encrypted_text
+    
+    def decrypt(self, text, key):
+        num_cols = (len(text) + key - 1) // key  # Số cột trong lưới
+        num_rows = key  # Số hàng trong lưới
+        num_shaded_boxes = (num_cols * num_rows) - len(text)  # Số ô trống cuối cùng
+
+        decrypted_text = [''] * num_cols
+        col, row = 0, 0
+
+        for symbol in text:
+            decrypted_text[col] += symbol
+            col += 1
+            
+            if col == num_cols or (col == num_cols - 1 and row >= num_rows - num_shaded_boxes):
+                col = 0
+                row += 1
+
+        return ''.join(decrypted_text)
---
+++ Lab_02/Cipher/vigenere/__init__.py
@@ -0,0 +1,1 @@
+from .vigener_cipher import VigenereCipher
